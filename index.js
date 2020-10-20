1208b01f713c295a08f5380561f502bb642fcc96
Re <50343845+Recal@users.noreply.github.com>
2020-10-20 15:01:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function toFixed(amount, value) {
+function toFixed(value, amount = 0) {
+
 	if( 
 	(typeof value != "string" || typeof value != "number") && value.toString().indexOf(".") == -1
 	) return null;
+
+	if(amount < 1 && amount != 0) return null;
+	if(amount == 0) return Math.round(value);
 
 	
 	value = value.toString();
@@ -13,7 +17,7 @@ function toFixed(amount, value) {
 		const numValue = newValue[0];
 		const pointValue = newValue[1];
 
-	
+		
 		if(amount > pointValue.length) return null;
 
 		for(let i = 0; i < amount; i++) {
@@ -24,4 +28,5 @@ function toFixed(amount, value) {
 
 		return Number(numValue + "." + newPointValue );
 	}
+	
 }
